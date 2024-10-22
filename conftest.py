124e224dfae987f433edd4513e9307d9153dd133
Andrey Tutunnikov <tt.a.s@yandex.ru>
2024-10-22 22:58:24 +0300

    изменены фикстуры - убраны создание книг и присвоение жанра

--- conftest.py
+++ conftest.py
@@ -1,22 +1,7 @@
 import pytest
-
 from main import BooksCollector
 
 @pytest.fixture
 def collector():
     collector = BooksCollector()
-    return collector
-
-@pytest.fixture
-def books_genre(collector):
-    collector.add_new_book('Кот колдун')
-    collector.add_new_book('Кот зомби')
-    collector.add_new_book('Кот Леопольд')
-    return collector
-
-@pytest.fixture
-def book_genre(collector, books_genre):
-    collector.set_book_genre('Кот колдун', 'Фантастика')
-    collector.set_book_genre('Кот зомби', 'Ужасы')
-    collector.set_book_genre('Кот Леопольд', 'Мультфильмы')
     return collector
